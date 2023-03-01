5333544bfb56569a2ede879fdae2f26bfeaa55a2
darianbla <darian1vega@gmail.com>
2023-03-01 14:48:45 +0200

    email flex

--- apps/email/cmps/EmailPreview.js
+++ apps/email/cmps/EmailPreview.js
@@ -2,9 +2,9 @@ export default {
     props: ['email'],
     template: `
         <article>
-            <h2>{{ email.subject }}</h2>
-            <h3>{{ email.body }}</h3>
-            <h3>{{ email.from }}</h3>
+            <h3>{{ email.subject }}</h2>
+            <h4>From: {{ email.from }}</h3>
+            <h2>{{ email.body }}</h3>
         </article>
     `
 }
