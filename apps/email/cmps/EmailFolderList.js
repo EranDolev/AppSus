ab37114e8cf7e5a20e7ab54d1bcaa68a4a0cfb12
darianbla <darian1vega@gmail.com>
2023-03-02 20:42:33 +0200

    pushing before 21:00

--- apps/email/cmps/EmailFolderList.js
+++ apps/email/cmps/EmailFolderList.js
@@ -6,9 +6,9 @@ export default {
             <nav class="nav-folder-list">
                 <ul class="folder-list-card">
                     <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink>
+                    <!-- <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink>
                     <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink>
-                    <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink>
-                    <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink>
+                    <RouterLink :to="'/apps/email/email-compose/'"><button class="btn-compose"><i class="fa-solid fa-pencil"></i> </button></RouterLink> -->
                 </ul>
             </nav>
         </div>
